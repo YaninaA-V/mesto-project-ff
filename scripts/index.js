9e02e7f5d4ae0cb999819badcdc94ff26f10f93d
YaninaA-V <JaninaVit92@yandex.ru>
2024-10-14 23:37:54 +0300

    Первый коммит

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
-// @todo: Темплейт карточки
+function createCard (name, link) {
+    const cardTemlate = document.querySelector('#card-template').content;
+    const cardElement = cardTemlate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    document.querySelector('.places__list').append(cardElement); 
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+deleteButton.addEventListener('click', function (event) {
+    event.target.closest('.places__item ').remove();
+   }); 
+}
+initialCards.forEach(elem => createCard(elem.name, elem.link));
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
