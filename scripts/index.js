51bc821fc63b41a30cea5d28416191e69f121baa
YaninaA-V <JaninaVit92@yandex.ru>
2024-10-15 22:23:24 +0300

    Второй коммит

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
-function createCard (name, link) {
+function createCard(name, link) {
     const cardTemlate = document.querySelector('#card-template').content;
     const cardElement = cardTemlate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').alt = name;
     cardElement.querySelector('.card__image').src = link;
     cardElement.querySelector('.card__title').textContent = name;
-    document.querySelector('.places__list').append(cardElement); 
     const deleteButton = cardElement.querySelector('.card__delete-button');
-deleteButton.addEventListener('click', function (event) {
-    event.target.closest('.places__item ').remove();
-   }); 
-}
+    deleteButton.addEventListener('click', deleteCard);
+    document.querySelector('.places__list').append(cardElement);   
+};
+
 initialCards.forEach(elem => createCard(elem.name, elem.link));
 
+function deleteCard(event) {
+    event.target.closest('.places__item').remove();
+}
 
